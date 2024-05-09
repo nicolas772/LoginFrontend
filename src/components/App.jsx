b6fa01a49d8451af517c1f427c71d2090e872ca7
nicolas772 <nicolas.arayau@sansano.usm.cl>
2024-05-09 15:21:02 -0400

    estilos para login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
-import '../css/App.css'
+import * as React from 'react'
+import Form from './Form'
 
 function App() {
-
   return (
     <>
-      <h1>Login Template</h1>
+      <div className='flex w-full h-screen'>
+        <div className='w-full flex items-center justify-center lg:w-1/2'>
+          <Form></Form>
+        </div>
+        <div className='hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200'>
+          <div className='w-60 h-60 bg-gradient-to-tr from-green-700 to-green-400 rounded-full animate-bounce'/>
+          <div className='w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg'></div>
+        </div>
+      </div>
     </>
   )
 }
